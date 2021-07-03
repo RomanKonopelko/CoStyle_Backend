7b814ec3b86d838a792cb06ec12964c713b5d131
RomanKonopelko <romank761@gmail.com>
2021-07-03 20:25:38 +0300

    feat: setup basic server logic

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-
-const contactsRouter = require("./routes/api/contacts");
-
 const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
@@ -11,8 +8,6 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-
-app.use("/api/transactions", contactsRouter);
 
 app.use((req, res) => {
   res.status(404).json({ status: "error", code: 404, message: "Not found" });

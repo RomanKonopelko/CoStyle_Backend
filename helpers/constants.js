4a7729ff792951c54383483f0dbcdf8763bc7713
RomanKonopelko <romank761@gmail.com>
2021-07-04 15:08:17 +0300

    feat: add starter transaction scheme

---
+++ helpers/constants.js
@@ -0,0 +1,12 @@
+CATEGORY_VALUES = {
+  main: "Основные",
+  food: "Еда",
+  car: "Машина",
+  development: "Развитие",
+  education: "Образование",
+  children: "Дети",
+  house: "Дом",
+  other: "Другое",
+};
+
+module.exports = { CATEGORY_VALUES };
---
+++ model/transactionScheme.js
@@ -0,0 +1,55 @@
+const { Schema, model, SchemaTypes } = require("mongoose");
+const mongoosePaginate = require("mongoose-paginate-v2");
+const { CATEGORY_VALUES } = require("../helpers/constants");
+
+const CATEGORIES = Object.values(CATEGORY_VALUES);
+
+const transactionSchema = new Schema(
+  {
+    category: {
+      type: String,
+      enum: CATEGORIES,
+      default: null,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+      default: null,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    owner: { type: SchemaTypes.ObjectId, ref: "user" },
+  },
+  {
+    versionKey: false,
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+    toObject: {
+      virtuals: true,
+      transform: function (doc, ret) {
+        delete ret._id;
+        return ret;
+      },
+    },
+  }
+);
+
+transactionSchema.virtual("info").get(function () {
+  return `This is transactions of ${this.name}`;
+});
+
+transactionSchemaSchema.plugin(mongoosePaginate);
+
+const Transaction = model("contact", contactSchema);
+
+module.exports = Contact;

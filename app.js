8e179fb5a4323dddd587d7827eccada2d9e458b3
RomanKonopelko <romank761@gmail.com>
2021-07-05 16:44:21 +0300

    feat: install swaggerUI, add swagger.json config

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 

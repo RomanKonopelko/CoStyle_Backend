7a49f11b041f5ddcf230f0cfdece1328ad128690
RomanKonopelko <romank761@gmail.com>
2021-07-22 19:38:58 +0300

    fix: dotenv import

--- views/unverifiedEmail/index.js
+++ views/unverifiedEmail/index.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 const { NETLIFY_URL } = process.env;
 
 setTimeout(function () {
--- views/verifiedEmail/index.js
+++ views/verifiedEmail/index.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 const { NETLIFY_URL } = process.env;
 
